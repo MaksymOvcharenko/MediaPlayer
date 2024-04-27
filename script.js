e7952720ad7b47044297f7b30be9876d3ba62f68
13DDC <150695013+13DDC@users.noreply.github.com>
2024-04-27 20:06:43 +0300

    ...

---
+++ script.js
@@ -0,0 +1,6 @@
+const playListSong = document.querySelector("#playlist-songs");
+console.log(playListSong);
+const play = document.querySelector("#play");
+console.log(play);
+const pause = document.querySelector("#pause");
+console.log(pause);
